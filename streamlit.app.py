c79a6a03c827de79e3bb651340812d94f1af0776
saram2001 <106420416+saram2001@users.noreply.github.com>
2022-05-28 10:39:25 +0530

    Update streamlit.app.py

--- streamlit.app.py
+++ streamlit.app.py
@@ -8,7 +8,7 @@ streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
 
 streamlit.text('🥗Kale,Spinach & Rocket smoothie')
 
-streamlit.text( 🐔'Hard-Boiled Free-Range Egg')
+streamlit.text( '🐔Hard-Boiled Free-Range Egg')
 
 streamlit.text('🥑🍞Avocado Toast')
 
