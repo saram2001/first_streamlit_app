ee34906a6baebbf791cae46cbfe4889fd2126955
saram2001 <106420416+saram2001@users.noreply.github.com>
2022-05-28 10:39:59 +0530

    Update streamlit.app.py

--- streamlit.app.py
+++ streamlit.app.py
@@ -36,7 +36,7 @@ streamlit.text(fruityvice_response)
 
 #New section to display fruityvice api response
 streamlit.header('Fruityvice Fruit advice!')
-fruity_choice = streamlit.text_input('What Fruit would you like informatiopn about?', 'apple')
+fruity_choice = streamlit.text_input('What Fruit would you like informatiopn about?', 'kiwi')
 streamlit.write('The user entered', fruity_choice)
 
 
