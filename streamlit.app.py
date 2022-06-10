8c3ea8842d3e32bbf1f9bb00c1fb41f96eee4309
saram2001 <106420416+saram2001@users.noreply.github.com>
2022-06-10 15:58:55 +0530

    Update streamlit.app.py

--- streamlit.app.py
+++ streamlit.app.py
@@ -50,6 +50,13 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 #output it the screen as a table
 streamlit.dataframe(fruityvice_normalized)
 
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
+
 # Allow the end user to add a fruit to the list
 add_my_fruit = streamlit.text_input('What fruit wouls you like information about?', 'jackfruit')
 streamlit.write('Thanks for adding', add_my_fruit)
